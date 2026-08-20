bfdf4d2866c221833bc123c9b674ca6d546fe8b6
Chaitanya Sharma <chaitanyavyas21@gmail.com>
2026-08-20 22:56:51 +0530

    Time: 6 ms (5.98%) | Memory: 47.1 MB (6.19%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.java
+++ 4177-reverse-string-prefix/reverse-string-prefix.java
@@ -1,13 +1,13 @@
 class Solution {
     public String reversePrefix(String s, int k) {
-       String  news="";
-        String nres="";
-        for(int i=k-1;i>=0;i--){
-            news += s.charAt(i);
+        String newe = "";
+        String now = "";
+        for(int i = k-1;i>=0;i--){
+            newe+=s.charAt(i);
         }
         for(int i=k;i<s.length();i++){
-            nres += s.charAt(i);
+            now+=s.charAt(i);
         }
-        return news+nres;
+        return newe+now;
     }
 }
